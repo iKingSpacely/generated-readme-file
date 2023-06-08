a1ac1002481d1f1550352dfbc0a0558c8695300e
Adam Colin <accolin89@gmail.com>
2023-06-07 19:47:08 -0500

    created the format for the readme file

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
+//below consts tie the inquirer npm to the index file and then fs will create the file source
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('generateMarkdown.js');
 
-inquirer
-    .prompt([
+//created the prompts for the user inputs
+const readMeQuestions = [
         {
             type: 'input',
             message: 'What is the name of your project?',
             name: 'Title',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
 
         {
             type: 'input',
             message: 'Please give a brief description of the project.',
             name: 'Description',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
         {
             type: 'input',
             message: 'Please describe installation instructions.',
             name: 'Installation',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
         {
             type: 'input',
             message: 'Please provide contribution guidelines if applicable.',
             name: 'Contribution Guidelines',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
         {
             type: 'input',
             message: 'Please provide usage examples if applicable',
             name: 'Usage',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
         {
             type: 'input',
             message: 'Please provide tests written for you application with examples if applicable.',
             name: 'Tests',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
 
         {
@@ -47,27 +41,77 @@ inquirer
             message: 'What license did you use on your repo?',
             name: 'license',
             choices: ['MIT License', 'Apache License 2.0', 'Mozilla Public License', 'Mozilla Public License 2.0'],
-            validate: (value) => {if(value){return true} else {return 'You forgot to choose a license!'}},
         },
 
         {
             type: 'input',
             message: 'What is your Github username?',
             name: 'Github Username',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
         },
 
         {
             type: 'input',
             message: 'What is your email address?',
             name: 'Email Address',
-            validate: (value) => {if(value){return true} else {return 'You forgot to enter something!'}},
+
         },
 
-    ])
+    ];
+
+//the below code will generate the readme content from the user inputs
+
+function createReadMe(userinput) {
+    return `
+# ${userinput.title}
+
+## Description
+${userinput.description}
+
+## Table of Contents
+${userinput.installation}
+${userinput.contributionguidelines}
+${userinput.usage}
+${userinput.tests}
+${userinput.license}
+${userinput.questions}
+
+## Installation
+${userinput.installation}
+
+## Contribution Guidelines
+${userinput.contributionguidelines}
+
+## Usage
+${userinput.usage}
+
+## Tests
+${userinput.tests}
+
+## License
+${userinput.license}
+
+## Questions
+For any questions, please reach out to on Github: ${userinput.questions}
+Or, you can contact me via email: ${userinput.email}
+`;
+}
+
+
+
+
         .then ((response) =>
             fs.writeFile('Readme.md', JSON.stringify(response),
             (error) => {
                 error ? console.log(error) : console.log("Nice, Good Job!");
             }
         ));
+
+function userPrompts() {
+    inquirer
+        .prompt(questions)
+
+        .then((answers) => {
+            const readMeInfo = generateReadMe(answers)
+        }
+        )
+}
